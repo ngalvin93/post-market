7bf9ea9baea317526879fb71af1da6f022c8a9f1
wangkai <wfszwk@qq.com>
2020-03-10 15:56:59 +0800

    post data to api

--- postmarket/form.js
+++ postmarket/form.js
@@ -1,29 +1,33 @@
 var fileCount = 0;
 var fileList = {};
 var idIndex = 0;
+var itemIndex=1;
 
 
 function addItemToTable () {
-    console.log('add item')
-    const table = document.getElementById('items-table')
+    console.log('add item');
+    const table = document.getElementById('items-table');
 
-    const currentRow = table.insertRow()
-    currentRow.setAttribute('class', 'form-row m-t-10')
+    const currentRow = table.insertRow();
+    currentRow.setAttribute('class', 'form-row m-t-10');
 
-    const itemInput = document.createElement('input')
-    itemInput.setAttribute('class', 'form-control itemNameInput')
+    const itemInput = document.createElement('input');
+    itemInput.setAttribute('class', 'form-control itemNameInput');
+    itemInput.setAttribute('id', 'itemName'+itemIndex);
 
     const amountInput = document.createElement('input')
-    amountInput.setAttribute('class', 'form-control itemAmountInput')
+    amountInput.setAttribute('class', 'form-control itemAmountInput');
+    amountInput.setAttribute('id', 'itemAmount'+itemIndex);
 
-    let currentCell = currentRow.insertCell()
-    currentCell.setAttribute('class', 'col-9')
-    currentCell.appendChild(itemInput)
+    let currentCell = currentRow.insertCell();
+    currentCell.setAttribute('class', 'col-9');
+    currentCell.appendChild(itemInput);
 
-    currentCell = currentRow.insertCell()
-    currentCell.setAttribute('class', 'col-3')
-    currentCell.appendChild(amountInput)
+    currentCell = currentRow.insertCell();
+    currentCell.setAttribute('class', 'col-3');
+    currentCell.appendChild(amountInput);
     $('.itemAmountInput').mask("#,##0.00", {reverse: true});
+    itemIndex++;
     GetTotal();
 }
 
@@ -71,12 +75,12 @@ function formatFileSize(size) {
 
 function readURL(file) {
     var extname = extnameFun(file.name).toLowerCase();
-    if ((extname == 'doc' || extname == 'docx' || extname == 'pdf' || extname == 'jpg' || extname == 'jepg')&&file.size<5*1024*1024) {
+    if ((extname == 'doc' || extname == 'docx' || extname == 'pdf' || extname == 'jpg' || extname == 'jepg') && file.size < 5 * 1024 * 1024) {
         var reader = new FileReader();
         reader.readAsDataURL(file);
         reader.onload = function () {
-            fileList[idIndex] = {"name": file.name, file: this.result};
-            $("#uploadFileList").append('<div class="form-control uploadItem m-t-10" id="fileItem' + idIndex + '">' + file.name + '('+formatFileSize(file.size)+')<img src="delete.png" style="width: 24px; float: right; cursor: pointer; margin-top:10px" onclick="removeFile('+idIndex+')" /></div>');
+            fileList[idIndex] = {"filename": file.name, file: this.result, filesize: file.size};
+            $("#uploadFileList").append('<div class="form-control uploadItem m-t-10" id="fileItem' + idIndex + '">' + file.name + '(' + formatFileSize(file.size) + ')<img src="delete.png" style="width: 24px; float: right; cursor: pointer; margin-top:10px" onclick="removeFile(' + idIndex + ')" /></div>');
             fileCount++;
             idIndex++;
 
@@ -103,6 +107,46 @@ function GetTotal() {
     });
 }
 
+
+function saveForm() {
+
+    var itemList=[];
+   for(var i=0;i<itemIndex;i++) {
+       itemList.push({name: $("#itemName" + i).val(), price: parseFloat($("#itemAmount" + i).val().replace(/,/g, ''))})
+   }
+
+    var postData = {
+        firstname: $("#first-name").val(),
+        lastname: $("#last-name").val(),
+        email: $("#email").val(),
+        phone: $("#phone").val(),
+        businessname: $("#business-name").val(),
+        stall: $("#stall-number").val(),
+        from: $("#select-form").val(),
+        price: parseFloat($("#amountTotal").val().replace(/,/g, '')),
+        itemlist:itemList,
+        filelist: fileList
+    }
+
+
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        contentType: "application/json;charset=UTF-8",
+        url: "http://localhost:8342/api/saveform",
+        data: JSON.stringify(postData),
+        success: function (data) {
+            if (data.result == 1) {
+
+            }
+            else {
+                alert(data.message);
+            }
+        },
+        error: function (e) {
+        }
+    });
+}
 
 
 
@@ -141,6 +185,9 @@ $(function(){
         }
     });
 
+    $("#submit-btn").click(function(){
+        saveForm();
+    })
 
 
 
